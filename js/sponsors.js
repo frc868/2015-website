a18e7f03ab4ab270429a3e12249aceb737984278
Vincent <tqm397@gmail.com>
2015-01-09 10:30:04 -0500

    Added function to load in a sponsors json file.

--- js/sponsors.js
+++ js/sponsors.js
@@ -1,6 +1,28 @@
 //This is JavaScript that will generate sponsors based on a .json file.
 //This will eliminate all sorts of hard-coded badness.
 
-function generateSponsorBox(){
+function generateSponsorBox(data){
 	alert("HI!");
+	var money = 1000000;
+	foreach(sponsor in sponsors){
+		sponsor.give(money);
+		techhounds.give(thank_you);
+		sponsor.receive(thank_you);
+	}
+	techhounds.makeIt(rain);
+}
+
+
+function requestData() {
+	$.ajax({
+		url: "sponsors.json",
+		cache: false,
+		async : true,
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			console.log(errorThrown);
+		},
+		success: function(data){
+			console.log(data);
+		}
+	});
 }
