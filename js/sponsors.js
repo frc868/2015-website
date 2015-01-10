da2f09520d50c7bcd51ece5afa1bdaccfcde1c1b
Vincent <tqm397@gmail.com>
2015-01-10 11:04:41 -0500

    Sponsors can now be generated based on what is inside the sponsors.json file.

--- js/sponsors.js
+++ js/sponsors.js
@@ -2,21 +2,80 @@
 //This will eliminate all sorts of hard-coded badness.
 
 function generateSponsorBox(data){
-	alert("HI!");
-	var money = 1000000;
+	var keys = Object.keys(data);
+	console.log(keys.length);
+	for (var i=0;i < keys.length;i++){
+		console.log(keys[i]);
+		var company = data[keys[i]];
+		console.log(company[0].name);
+
+		var div = document.createElement("div");
+		var body = document.getElementById("body");
+
+		div.id = keys[i];
+		div.className = "sponsorDiv";
+		if(company[0].description != "none"){
+			div.setAttribute("onclick","test(this)")
+			div.setAttribute("data-toggle","modal")
+			div.setAttribute("data-target","#sponsorModal")
+		}
+		if(company[0].logo != "none"){
+			var img = document.createElement("img");
+			img.src = company[0].logo;
+			img.alt = company[0].name;
+			div.appendChild(img);
+		}
+		else {
+			var h3 = document.createElement("h3");
+			h3.innerHTML = company[0].name;
+			
+			if(company[0].description != "none"){
+				div.style.cursor = "pointer";
+			}
+			
+			div.appendChild(h3);
+		}
+
+		body.appendChild(div);
+	}
 }
 
+function test(sponsorDiv){
+	console.log(sponsorDiv);
+	var id = sponsorDiv.id;
+	var company = data[id];
+	console.log(company[0].name);
+
+	var modalTitle = document.getElementById("modal-title");
+	var modalBody = document.getElementById("modal-body");
+	var modalFooter = document.getElementById("modal-footer");
+	
+	modalTitle.innerHTML = company[0].name;
+	modalBody.innerHTML = company[0].description;
+	modalFooter.innerHTML = "<button class='btn' data-dismiss='modal' type='button'>Close</button>";
+	if(company[0].website != "none"){
+		var websiteButton = document.createElement("a");
+		websiteButton.className = "btn website";
+		websiteButton.target = "_blank";
+		websiteButton.href = company[0].website;
+		websiteButton.innerHTML = "Website";
+		modalFooter.appendChild(websiteButton);
+	}
+}
 
 function requestData() {
 	$.ajax({
 		url: "sponsors.json",
+		dataType: "json",
 		cache: false,
 		async: true,
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			console.log(errorThrown);
 		},
-		success: function(data){
+		success: function(html){
+			data = html;
 			console.log(data);
+			generateSponsorBox(data);
 		}
 	});
 }
