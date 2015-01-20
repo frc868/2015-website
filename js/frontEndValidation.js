5397047a19fcde521d55a66997b2380c3cbe4e41
Vincent <tqm397@gmail.com>
2015-01-20 16:45:28 -0500

    Added some sign in files.

---
+++ js/frontEndValidation.js
@@ -0,0 +1,179 @@
+var data = [
+	{ "key":"500003553", "first":"vincent", "last":"mai" },
+	{ "key":"600002162", "first":"tiger", "last":"huang" },
+	{ "key":"500005498", "first":"atif", "last":"niyaz" },
+	{ "key":"500011800", "first":"sameer", "last":"leley" },
+	{ "key":"500005849", "first":"joseph", "last":"dobie" },
+	{ "key":"500005968", "first":"ben", "last":"cunningham" },
+	{ "key":"500019672", "first":"cynthia", "last":"chang" },
+	{ "key":"500011154", "first":"michael", "last":"dobie" },
+	{ "key":"500008253", "first":"allison", "last":"cunningham" },
+	{ "key":"500010432", "first":"kevin", "last":"chen" },
+	{ "key":"600003377", "first":"jacob", "last":"fecht" },
+	{ "key":"600081291", "first":"aryaman", "last":"gupta" },
+	{ "key":"500005510", "first":"jake", "last":"patterson" },
+	{ "key":"500022435", "first":"connor", "last":"dunn" },
+	{ "key":"500012055", "first":"jackson", "last":"otto" },
+	{ "key":"500019384", "first":"riley", "last":"borgard" },
+	{ "key":"500016383", "first":"megan", "last":"singer" },
+	{ "key":"500011185", "first":"brandon", "last":"tran" },
+	{ "key":"500011902", "first":"joseph", "last":"vinyard" },
+	{ "key":"600080262", "first":"linnea", "last":"schultz" },
+	{ "key":"500014666", "first":"kalen", "last":"rudd" },
+	{ "key":"500011175", "first":"eric", "last":"zhu" },
+	{ "key":"600087907", "first":"elijah", "last":"jones" },
+	{ "key":"(>'.')>NUMBERS<('.'<)", "first":"david", "last":"murzyn" },
+	{ "key":"500008398", "first":"carolina", "last":"hernandez" },
+	{ "key":"500005587", "first":"matthew", "last":"bonini" },
+	{ "key":"500011909", "first":"luke", "last":"obrique" },
+	{ "key":"500019380", "first":"zachary", "last":"fleckenstein" },
+	{ "key":"600003639", "first":"jessica", "last":"tian" },
+	{ "key":"500004655", "first":"anna", "last":"fang" },
+	{ "key":"500004639", "first":"john", "last":"wimmer" },
+	{ "key":"500014507", "first":"mara", "last":"fattah" },
+	{ "key":"600084081", "first":"gayatri", "last":"balasubramanian" },
+	{ "key":"500022287", "first":"shaurya", "last":"doger" },
+	{ "key":"500006176", "first":"calvin", "last":"henry" },
+	{ "key":"600082609", "first":"karan", "last":"manektala" },
+	{ "key":"600002182", "first":"hari", "last":"rao" },
+	{ "key":"500010837", "first":"connor", "last":"mahoney" },
+	{ "key":"500007876", "first":"chris", "last":"kannmacher" },
+	{ "key":"500005767", "first":"matthew", "last":"hussey" },
+	{ "key":"500005767", "first":"matthew", "last":"hussey" },
+	{ "key":"500008474", "first":"jaymee", "last":"stout" },
+	{ "key":"500007765", "first":"jacob", "last":"rettig" },
+	{ "key":"500003340", "first":"jacob", "last":"swiezy" },
+	{ "key":"600081262", "first":"vikas", "last":"maturi" },
+	{ "key":"600001543", "first":"abhinav", "last":"ramkumar" },
+	{ "key":"500011065", "first":"deepa", "last":"jayasankar" },
+	{ "key":"500014596", "first":"molly", "last":"wardlow" },
+	{ "key":"500003548", "first":"blake", "last":"loncharich" },
+	{ "key":"500005582", "first":"aaron", "last":"ryker" },
+	{ "key":"500008713", "first":"garrett", "last":"rose" },
+	{ "key":"500017249", "first":"garrett", "last":"ferry" },
+	{ "key":"500011644", "first":"jonah", "last":"jordan" },
+	{ "key":"500013931", "first":"sailesh", "last":"kandula" },
+	{ "key":"500016892", "first":"kevin", "last":"zhou" },
+	{ "key":"500017150", "first":"evan", "last":"dexter" },
+	{ "key":"500014454", "first":"jack", "last":"engledow" },
+	{ "key":"500010666", "first":"alec", "last":"booth" },
+	{ "key":"500006079", "first":"jimmy", "last":"williams" },
+	{ "key":"500011292", "first":"andrew", "last":"dennison" },
+	{ "key":"600002360", "first":"jessica", "last":"earnhardt" },
+	{ "key":"500014984", "first":"buster", "last":"curtis" },
+	{ "key":"500014186", "first":"jacob", "last":"conn" },
+	{ "key":"500011081", "first":"jack", "last":"roy" },
+	{ "key":"600085426", "first":"madison", "last":"rosen" },
+	{ "key":"500011797", "first":"sam", "last":"kim" },
+	{ "key":"600087113", "first":"eduardo", "last":"pons" },
+	{ "key":"600086468", "first":"allison", "last":"earnhardt" },
+	{ "key":"500008234", "first":"matthew", "last":"simons" },
+	{ "key":"500003463", "first":"carson", "last":"piedmonte" },
+	{ "key":"500012737", "first":"julien", "last":"zhu" },
+	{ "key":"500010770", "first":"pranav", "last":"sriram" },
+	{ "key":"500007447", "first":"ethan", "last":"counen" },
+	{ "key":"500013614", "first":"ryan", "last":"carlton" },
+	{ "key":"500006011", "first":"zachary", "last":"simpson" },
+	{ "key":"500016934", "first":"aneesh", "last":"luthra" },
+	{ "key":"500011012", "first":"joshua", "last":"chu" },
+	{ "key":"500017060", "first":"caleb", "last":"smith" },
+	{ "key":"500017940", "first":"gengli", "last":"ding" },
+	{ "key":"600082482", "first":"elliot", "last":"choy" },
+	{ "key":"500003586", "first":"daniel", "last":"willeman" },
+	{ "key":"600087575", "first":"zachary", "last":"harris" },
+	{ "key":"500002651", "first":"parker", "last":"courter" },
+	{ "key":"500002396", "first":"nick", "last":"cooney" },
+	{ "key":"500016753", "first":"nick", "last":"shelton" },
+	{ "key":"500010870", "first":"jerome", "last":"sendon" },
+	{ "key":"500010731", "first":"ayon", "last":"mitra" },
+	{ "key":"500018882", "first":"dylan", "last":"waddell" },
+	{ "key":"600085020", "first":"erik", "last":"peterson" },
+	{ "key":"500005659", "first":"andrew", "last":"bass" },
+	{ "key":"500006493", "first":"jonathan", "last":"chen" },
+	{ "key":"500007955", "first":"clayton", "last":"detke" },
+	{ "key":"500007981", "first":"garrett", "last":"thompson" },
+	{ "key":"500005683", "first":"scott", "last":"yarling" },
+	{ "key":"500007636", "first":"adam", "last":"foster" },
+	{ "key":"500016757", "first":"nathan", "last":"robb" },
+	{ "key":"600083037", "first":"yunping", "last":"wang" },
+	{ "key":"500007286", "first":"stanley", "last":"liu" },
+	{ "key":"500007760", "first":"austin", "last":"weaver" },
+	{ "key":"500008621", "first":"mchale", "last":"fish" },
+	{ "key":"600082797", "first":"terry", "last":"zhou" },
+	{ "key":"500012078", "first":"maria", "last":"bashmakov" },
+	{ "key":"500006158", "first":"alex", "last":"fleig" },
+	{ "key":"500006974", "first":"john", "last":"flynn" },
+	{ "key":"500020255", "first":"joseph", "last":"seo" },
+	{ "key":"500009736", "first":"richard", "last":"du" },
+	{ "key":"500007810", "first":"case", "last":"evanson" },
+	{ "key":"600087893", "first":"noah", "last":"pilcher" },
+	{ "key":"500011610", "first":"zachary", "last":"lamear" },
+	{ "key":"500016796", "first":"leo", "last":"soyfer" },
+	{ "key":"500008293", "first":"cole", "last":"rohrabaugh" },
+	{ "key":"500005560", "first":"jarod", "last":"throckmorton" },
+	{ "key":"500012001", "first":"thomas", "last":"xuan" },
+	{ "key":"500020142", "first":"christopher", "last":"snelling" },
+	{ "key":"600082901", "first":"braxton", "last":"reichard" },
+	{ "key":"500016525", "first":"pelle", "last":"wrammerfors" },
+	{ "key":"N/A", "first":"matthew", "last":"tidwell" }
+];
+
+function validate() {
+  var first1 = document.getElementById("first").value,
+    last1 = document.getElementById("last").value,
+    sid1 = document.getElementById("sid").value;
+  var first = first1.trim().toLowerCase(), last = last1.trim().toLowerCase(),
+    key = sid1.toString(), firstRange = 1, lastRange = 1;
+  for(i = 0; i < data.length; i++) {
+    if(key === data[i].key && levDist(first, data[i].first) <= firstRange &&
+        levDist(last, data[i].last) <= lastRange) {
+      document.getElementById("ss-submit").disabled = false;
+      document.getElementById("first").value = capitalise(data[i].first);
+      document.getElementById("last").value = capitalise(data[i].last);
+      return;
+    }
+  }
+  document.getElementById("ss-submit").disabled = true;
+  return;
+}
+
+function levDist(s,t) {
+  // degenerate cases
+  if (s == t) return 0;
+  if (s.length === 0) return t.length;
+  if (t.length === 0) return s.length;
+  
+  // create two work vectors of integer distances
+  var v0 = new Array(t.length + 1);
+  var v1 = new Array(t.length + 1);
+  
+  // initialize v0 (the previous row of distances)
+  // this row is A[0][i]: edit distance for an empty s
+  // the distance is just the number of characters to delete from t
+  for (var i = 0; i < v0.length; i++)
+      v0[i] = i;
+  
+  for (var i = 0; i < s.length; i++) {
+      // calculate v1 (current row distances) from the previous row v0
+      
+      // first element of v1 is A[i+1][0]
+      //   edit distance is delete (i+1) chars from s to match empty t
+      v1[0] = i + 1;
+      
+      // use formula to fill in the rest of the row
+      for (var j = 0; j < t.length; j++) {
+          var cost = (s[i] == t[j]) ? 0 : 1;
+          v1[j + 1] = Math.min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost);
+        }
+      
+      // copy v1 (current row) to v0 (previous row) for next iteration
+      for (var j = 0; j < v0.length; j++)
+          v0[j] = v1[j];
+    }
+  
+  return v1[t.length];
+}
+
+function capitalise(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
