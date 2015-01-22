af1f7bb60529c56087e74544636467f3c8e83de3
Vincent <tqm397@gmail.com>
2015-01-22 00:05:26 -0500

    Updated sponsors.

--- js/sponsors.js
+++ js/sponsors.js
@@ -71,7 +71,7 @@ function test(sponsorDiv){
 
 function requestData() {
 	$.ajax({
-		url: "sponsors.json",
+		url: "http://www.techhounds.com/data/uploads/files-and-forms/sponsors.json",
 		dataType: "json",
 		cache: false,
 		async: true,
