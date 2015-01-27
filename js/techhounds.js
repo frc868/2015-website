1db6e4f53033c69fe7a843ef2e7b73611a124f74
Vincent <tqm397@gmail.com>
2015-01-27 10:22:52 -0500

    Added some js transitions to front page elements.

--- js/techhounds.js
+++ js/techhounds.js
@@ -11,3 +11,23 @@ $('.navbar').on('affix.bs.affix', function () {
 $('.navbar').on('affix-top.bs.affix', function () {
     $('.navbar + .bodyContainer').css('margin-top', 0);
 });
+
+$(window).on("load",function() {
+    function fade() {
+        $('.clearfix').each(function() {
+            /* Check the location of each desired element */
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+            
+            /* If the object is completely visible in the window, fade it in */
+            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+                if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+            }
+            /*else { //object goes out of view (scrolling up)
+                if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+            }*/
+        });
+    }
+    fade(); //Fade in completely visible elements during page-load
+    $(window).scroll(function() {fade();}); //Fade in elements during scroll
+});
