3ca0ca57672eb49a52f3d2862337769e41c4ecde
Vincent <tqm397@gmail.com>
2015-01-28 07:57:09 -0500

    Moved around some js. Fixed a typo.

--- js/techhounds.js
+++ js/techhounds.js
@@ -31,3 +31,23 @@ $(window).on("load",function() {
     fade(); //Fade in completely visible elements during page-load
     $(window).scroll(function() {fade();}); //Fade in elements during scroll
 });
+blueimp.Gallery(
+	document.getElementById('links').getElementsByTagName('a'),
+	{
+		container: '#home-carousel',
+		carousel: true,
+		startSlideshow: true,
+		slideshowInterval: 7000,
+		onslide: function (index, slide) {
+
+			var text = this.list[index].getAttribute('data-description'),
+			node = this.container.find('.description');
+			var link = this.list[index].getAttribute('data-href'),
+			node = this.container.find('.description');
+			node.empty();
+			if (text) {
+				node[0].appendChild(document.createTextNode(text));
+			}
+			node[0].href = link;
+		}
+	});
